44ac4c23e1fb2801ccfeaffdefebd85f5ab51b24
iris <iris.felshman@gmail.com>
2026-02-16 15:37:57 -0600

    update to version 0.0.2

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -2,80 +2,42 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
-class MatrixFMSynthAudioProcessorEditor : public juce::AudioProcessorEditor, public juce::Timer
-{
+class MatrixFMSynthAudioProcessorEditor : public juce::AudioProcessorEditor {
 public:
     MatrixFMSynthAudioProcessorEditor (MatrixFMSynthAudioProcessor&);
     ~MatrixFMSynthAudioProcessorEditor() override;
-
     void paint (juce::Graphics&) override;
     void resized() override;
-    void timerCallback() override;
-
+    void updateScale();
 private:
     MatrixFMSynthAudioProcessor& audioProcessor;
-
-    // Groups
-    juce::GroupComponent envelopeGroup;
-    juce::GroupComponent toolsGroup;
-    juce::GroupComponent matrixGroup;
-    juce::GroupComponent masterGroup;
-
-    // Header Controls
-    juce::ComboBox uiScaleMenu;
-    juce::TextButton saveBtn { "SAVE" }, loadBtn { "LOAD" }, initBtn { "INIT" };
-    juce::TextButton randomBtn { "RANDOMIZE MATRIX" };
-    juce::Label visualizerLabel;
-
-    // Master Section
-    juce::Slider masterSlider;
-    juce::Label masterLabel;
+    struct ModRow {
+        juce::ComboBox source, dest; juce::Slider amount;
+        std::unique_ptr<juce::AudioProcessorValueTreeState::ComboBoxAttachment> srcAtt, destAtt;
+        std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> amtAtt;
+    };
+    juce::GroupComponent envGroup, lfoGroup, matrixGroup, toolsGroup, masterGroup, modGroup;
+    juce::Label adsrTitle1, adsrTitle2, visualizerLabel, masterLabel, lfoLabels[2];
+    // In Private section of MatrixFMSynthAudioProcessorEditor
+    juce::Label envLabels[8]; // A, D, S, R for both envs
+    juce::Label lfoParamLabels[6]; // Rate, Amt, Depth for both LFOs
+    juce::Label opHeaderLabels[9]; // For: Gain, Env, Ratio, Mode, Context, Context2, Track, Matrix, Out
+    juce::Label opRowNumbers[6];   // For: OP 1, OP 2, etc.
+    juce::Slider masterSlider; juce::TextButton saveBtn{"SAVE"}, loadBtn{"LOAD"}, initBtn{"INIT"}, randModBtn{"RAND MOD"};
+    juce::ComboBox uiScaleMenu; juce::ResizableCornerComponent resizer;
+    juce::Slider adsr1Sliders[4], adsr2Sliders[4], lfoFreq[2], lfoAmt[2], lfoDirectDepth[2];
+    juce::ComboBox lfoWave[2], lfoDirectDest[2]; juce::ToggleButton lfoSyncBtn[2], envRetrigBtn[2];
+    juce::OwnedArray<ModRow> modRows;
+    juce::OwnedArray<juce::Slider> matrixKnobs, opGains, opGainsOut, opRatios, opContext1, opContext2, opTrack;
+    juce::OwnedArray<juce::ComboBox> opModes, opEnvSelect;
+    juce::OwnedArray<juce::Label> rowLabels;
+    juce::OwnedArray<juce::Label> opContextLabels1; // Labels for C1
+    juce::OwnedArray<juce::Label> opContextLabels2; // Labels for C2
+    juce::OwnedArray<juce::Label> opTrackLabels;   // Labels for Keytrack
+    juce::OwnedArray<juce::Label> matrixValueLabels;
     std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> masterAtt;
-
-    // ADSR
-    juce::Slider adsrSliders[4];
-    juce::Label adsrLabels[4];
-    std::unique_ptr<juce::AudioProcessorValueTreeState::SliderAttachment> adsrAtts[4];
-
-    // Operator Row Components
-    juce::OwnedArray<juce::Slider> opGains;        // Serial "Volume"
-    juce::OwnedArray<juce::Label> volLabels;      // "VOL" Labels
-    
-    juce::OwnedArray<juce::Slider> opRatios;
-    juce::OwnedArray<juce::ComboBox> opModes;
-    
-    juce::OwnedArray<juce::Slider> opContextKnobs1;
-    juce::OwnedArray<juce::Label> contextLabels1;
-    
-    juce::OwnedArray<juce::Slider> opContextKnobs2;
-    juce::OwnedArray<juce::Label> contextLabels2;
-
-    juce::OwnedArray<juce::Slider> matrixKnobs;
-    
-    juce::OwnedArray<juce::Slider> opGainsOut;     // Parallel "Volume Out"
-    juce::OwnedArray<juce::Label> volOutLabels;   // "OUT" Labels
-
-    juce::Label rowLabels[6];
-    juce::Label colLabels[6];
-
-    // Simple Level Meter
-    struct SimpleMeter : public juce::Component {
-        float level = 0.0f;
-        void setLevel(float l) { level = l; repaint(); }
-        void paint(juce::Graphics& g) override {
-            g.fillAll(juce::Colours::black);
-            g.setColour(juce::Colours::green.withAlpha(0.8f));
-            g.fillRect(0, (int)(getHeight()*(1.0f-level)), getWidth(), (int)(getHeight()*level));
-        }
-    } meter;
-
-    // Attachments
-    juce::OwnedArray<juce::AudioProcessorValueTreeState::SliderAttachment> atts;
-    juce::OwnedArray<juce::AudioProcessorValueTreeState::ComboBoxAttachment> modeAtts;
-
-    // Window Management
-    juce::ResizableCornerComponent resizer;
-    juce::ComponentBoundsConstrainer constrainer;
-
+    juce::OwnedArray<juce::AudioProcessorValueTreeState::SliderAttachment> sAtts;
+    juce::OwnedArray<juce::AudioProcessorValueTreeState::ButtonAttachment> bAtts;
+    juce::OwnedArray<juce::AudioProcessorValueTreeState::ComboBoxAttachment> cAtts;
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MatrixFMSynthAudioProcessorEditor)
 };
--- Source/SynthCore.h
+++ Source/SynthCore.h
@@ -1,183 +1,135 @@
 #pragma once
-
 #include <JuceHeader.h>
 
-/** * VoiceParams must be defined at the very top so that the 
- * FMVoice class knows what it is when it tries to use it.
- */
-struct VoiceParams {
-    float adsr[4];       
-    int modes[6];        
-    float ratios[6];     
-    float gains[6];      // Serial "Volume" (to next op)
-    float gainsOut[6];   // Parallel "Volume Out" (direct to master)
-    float res[6];        
-    float res2[6];       
-    float matrix[6][6];  
+struct LFOParams {
+    float freq = 1.0f;
+    float amount = 0.0f;
+    int wave = 0; // 0: Sine, 1: Tri, 2: Saw, 3: Sqr
+    bool sync = true;
 };
 
-/**
- * Individual Operator: Oscillators and Filters
- */
+struct VoiceParams {
+    float adsr1[4], adsr2[4];
+    int opAdsrSource[6]; 
+    bool envRetrigger[2];
+    int modes[6];
+    float ratios[6], gains[6], gainsOut[6], res[6], res2[6], kbTrack[6], matrix[6][6];
+    LFOParams lfos[2];
+    float modWheelValue = 0.0f;
+    int modSource[8], modDest[8];
+    float modAmount[8];
+    int lfoDirectDest[2];
+    float lfoDirectDepth[2];
+};
+
 class Operator {
 public:
-    enum Mode { Sine = 0, LoPass, Comb };
-
     void prepare(double sr, int samplesPerBlock) {
         sampleRate = sr;
         juce::dsp::ProcessSpec spec { sr, (juce::uint32)samplesPerBlock, 1 };
-        
         filter.prepare(spec);
         filter.setType(juce::dsp::StateVariableTPTFilterType::lowpass);
-        
         delayLine.prepare(spec);
-        delayLine.setMaximumDelayInSamples(sr * 0.2); // Slightly larger buffer for low freq comb
-        
+        delayLine.setMaximumDelayInSamples(sr * 0.5); 
         reset();
     }
+    void reset() { phase = 0.0f; filter.reset(); delayLine.reset(); }
 
-    void reset() {
-        phase = 0.0f;
-        filter.reset();
-        delayLine.reset();
-    }
-
-    float process(float input, float hz, float pMod, float c1, float c2, int modeIdx) {
-        Mode m = static_cast<Mode>(modeIdx);
-        
-        if (m == Sine) {
-            float phaseOffset = c1 * juce::MathConstants<float>::twoPi;
-            float out = std::sin(phase + pMod + phaseOffset);
-            
+    float process(float input, float hz, float pMod, float c1, float c2, float keyTrack, float noteHz, int modeIdx) {
+        if (modeIdx == 0) { // Sine
+            float out = std::sin(phase + pMod + (c1 * juce::MathConstants<float>::twoPi));
             phase += (juce::MathConstants<float>::twoPi * hz) / (float)sampleRate;
             if (phase >= juce::MathConstants<float>::twoPi) phase -= juce::MathConstants<float>::twoPi;
-            
-            return out + input; 
-        }
-        
-        if (m == LoPass) {
-            float cutoff = juce::jlimit(20.0f, 20000.0f, juce::jmap(c1, 20.0f, 20000.0f));
-            filter.setCutoffFrequency(cutoff);
+            return out + input;
+        } else if (modeIdx == 1) { // LoPass
+            float finalCutoff = juce::jlimit(20.0f, 20000.0f, juce::jmap(c1, 20.0f, 20000.0f) + (noteHz - 261.63f) * keyTrack);
+            filter.setCutoffFrequency(finalCutoff);
             filter.setResonance(juce::jlimit(0.1f, 10.0f, (c2 * 9.9f) + 0.1f));
             return filter.processSample(0, input);
-        }
-
-        if (m == Comb) {
-            float feedback = juce::jlimit(0.0f, 0.98f, c1);
-            float combHz = juce::jlimit(20.0f, 10000.0f, juce::jmap(c2, 20.0f, 10000.0f));
-            float delaySamples = (float)sampleRate / combHz;
-            
-            delayLine.setDelay(delaySamples);
+        } else { // Comb
+            delayLine.setDelay((float)sampleRate / juce::jlimit(20.0f, 18000.0f, juce::jmap(c2, 20.0f, 10000.0f)));
             float delayed = delayLine.popSample(0);
-            delayLine.pushSample(0, input + (delayed * feedback));
+            delayLine.pushSample(0, input + (delayed * juce::jlimit(0.0f, 0.98f, c1)));
             return input + delayed;
         }
-
-        return input;
     }
-
 private:
-    double sampleRate = 44100.0;
-    float phase = 0.0f;
+    double sampleRate = 44100.0; float phase = 0.0f;
     juce::dsp::StateVariableTPTFilter<float> filter;
     juce::dsp::DelayLine<float> delayLine { 50000 };
 };
 
-/**
- * Standard JUCE Sound definition
- */
 struct FMSound : public juce::SynthesiserSound {
     FMSound() {}
     bool appliesToNote (int) override { return true; }
     bool appliesToChannel (int) override { return true; }
 };
 
-/**
- * The Voice: Manages the chain of 6 Operators
- */
 class FMVoice : public juce::SynthesiserVoice {
 public:
-    FMVoice() {
-        for (int i = 0; i < 6; ++i) 
-            ops[i] = std::make_unique<Operator>();
-    }
-
-    bool canPlaySound (juce::SynthesiserSound* sound) override {
-        return dynamic_cast<FMSound*> (sound) != nullptr;
-    }
-
-    void prepare(double sr, int samplesPerBlock) {
-        adsr.setSampleRate(sr);
-        for (auto& op : ops) op->prepare(sr, samplesPerBlock);
-    }
-
-    void setCurrentParams(const VoiceParams& p) { 
-        params = p; 
-    }
+    FMVoice() { for (int i = 0; i < 6; ++i) ops[i] = std::make_unique<Operator>(); }
+    bool canPlaySound (juce::SynthesiserSound* s) override { return dynamic_cast<FMSound*>(s) != nullptr; }
+    void prepare(double sr, int samplesPerBlock) { adsr1.setSampleRate(sr); adsr2.setSampleRate(sr); for (auto& o : ops) o->prepare(sr, samplesPerBlock); }
+    void setCurrentParams(const VoiceParams& p) { params = p; }
 
     void startNote (int midiNoteNumber, float velocity, juce::SynthesiserSound*, int) override {
         noteHz = (float)juce::MidiMessage::getMidiNoteInHertz(midiNoteNumber);
-        
-        juce::ADSR::Parameters adsrParams;
-        adsrParams.attack  = params.adsr[0];
-        adsrParams.decay   = params.adsr[1];
-        adsrParams.sustain = params.adsr[2];
-        adsrParams.release = params.adsr[3];
-        
-        adsr.setParameters(adsrParams);
-        for (auto& op : ops) op->reset();
-        adsr.noteOn();
+        juce::ADSR::Parameters p1, p2;
+        p1.attack = params.adsr1[0]; p1.decay = params.adsr1[1]; p1.sustain = params.adsr1[2]; p1.release = params.adsr1[3];
+        p2.attack = params.adsr2[0]; p2.decay = params.adsr2[1]; p2.sustain = params.adsr2[2]; p2.release = params.adsr2[3];
+        adsr1.setParameters(p1); adsr2.setParameters(p2);
+        if (params.envRetrigger[0]) adsr1.reset();
+        if (params.envRetrigger[1]) adsr2.reset();
+        for (int i=0; i<2; ++i) if(params.lfos[i].sync) lfoPhases[i] = 0.0f;
+        for (auto& o : ops) o->reset();
+        adsr1.noteOn(); adsr2.noteOn();
     }
 
-    void stopNote (float, bool allowTailOff) override {
-        adsr.noteOff();
-        if (!allowTailOff) clearCurrentNote();
-    }
+    void stopNote (float, bool allowTailOff) override { adsr1.noteOff(); adsr2.noteOff(); if (!allowTailOff) clearCurrentNote(); }
 
     void renderNextBlock (juce::AudioBuffer<float>& outputBuffer, int startSample, int numSamples) override {
-        if (!adsr.isActive()) return;
-
         for (int s = 0; s < numSamples; ++s) {
-            float runningChainBuffer = 0.0f;
-            float parallelOutputSum = 0.0f; 
-            float opOuts[6] = { 0.0f }; 
+            float lfoVals[2] = {0.0f, 0.0f};
+            for(int k=0; k<2; ++k) {
+                float raw = std::sin(lfoPhases[k]);
+                if(params.lfos[k].wave == 1) raw = (std::asin(raw) * 0.6366f);
+                else if(params.lfos[k].wave == 2) raw = (lfoPhases[k] / juce::MathConstants<float>::pi) - 1.0f;
+                else if(params.lfos[k].wave == 3) raw = (lfoPhases[k] < juce::MathConstants<float>::pi) ? 1.0f : -1.0f;
+                lfoVals[k] = raw * params.lfos[k].amount;
+                lfoPhases[k] += (juce::MathConstants<float>::twoPi * params.lfos[k].freq) / getSampleRate();
+                if(lfoPhases[k] >= juce::MathConstants<float>::twoPi) lfoPhases[k] -= juce::MathConstants<float>::twoPi;
+            }
+
+            float modOffsets[55] = {0.0f};
+            for (int i=0; i<8; ++i) {
+                if (params.modDest[i] == 0) continue;
+                float src = (params.modSource[i] == 0) ? lfoVals[0] : (params.modSource[i] == 1 ? lfoVals[1] : params.modWheelValue);
+                modOffsets[params.modDest[i]] += (src * params.modAmount[i]);
+            }
+            for (int i=0; i<2; ++i) if (params.lfoDirectDest[i] > 0) modOffsets[params.lfoDirectDest[i]] += (lfoVals[i] * params.lfoDirectDepth[i]);
+
+            float env1 = adsr1.getNextSample(), env2 = adsr2.getNextSample();
+            float chain = 0.0f, sum = 0.0f, opOuts[6] = {0.0f};
 
             for (int i = 0; i < 6; ++i) {
-                float phaseMod = 0.0f;
-                for (int j = 0; j < 6; ++j) {
-                    phaseMod += opOuts[j] * params.matrix[i][j];
-                }
-
-                float hz = noteHz * params.ratios[i];
-
-                // Process if there is active gain in either output path
-                if (params.gains[i] > 0.0001f || params.gainsOut[i] > 0.0001f) {
-                    float processed = ops[i]->process(runningChainBuffer, hz, phaseMod, 
-                                                     params.res[i], params.res2[i], params.modes[i]);
-                    
-                    parallelOutputSum += processed * params.gainsOut[i];
-                    runningChainBuffer = processed * params.gains[i];
-                    opOuts[i] = processed;
-                }
+                float pMod = 0.0f;
+                for (int j = 0; j < 6; ++j) pMod += opOuts[j] * juce::jlimit(-1.0f, 1.0f, params.matrix[i][j] + modOffsets[19 + (i*6) + j]);
+                float curEnv = (params.opAdsrSource[i] == 0) ? env1 : env2;
+                float curG = juce::jlimit(0.0f, 1.0f, params.gains[i] + modOffsets[i+1]);
+                float curR = juce::jlimit(0.1f, 20.0f, params.ratios[i] + modOffsets[i+7]);
+                float curC1 = juce::jlimit(0.0f, 1.0f, params.res[i] + modOffsets[i+13]);
+                float out = ops[i]->process(chain, noteHz * curR, pMod, curC1, params.res2[i], params.kbTrack[i], noteHz, params.modes[i]);
+                sum += out * params.gainsOut[i] * curEnv;
+                chain = out * curG * curEnv;
+                opOuts[i] = out;
             }
-
-            float adsrVal = adsr.getNextSample();
-            float finalSample = (parallelOutputSum + runningChainBuffer) * adsrVal * 0.2f;
-
-            for (int ch = 0; ch < outputBuffer.getNumChannels(); ++ch) {
-                outputBuffer.addSample(ch, startSample + s, finalSample);
-            }
-
-            if (adsrVal <= 0.001f && !adsr.isActive()) clearCurrentNote();
+            float final = (sum + chain) * 0.2f;
+            for (int ch = 0; ch < outputBuffer.getNumChannels(); ++ch) outputBuffer.addSample(ch, startSample + s, final);
+            if (!adsr1.isActive() && !adsr2.isActive()) clearCurrentNote();
         }
     }
-
     void pitchWheelMoved (int) override {}
-    void controllerMoved (int, int) override {}
-
+    void controllerMoved (int num, int val) override { if (num == 1) params.modWheelValue = (float)val / 127.0f; }
 private:
-    std::unique_ptr<Operator> ops[6]; 
-    juce::ADSR adsr;
-    VoiceParams params;
-    float noteHz = 0.0f;
+    std::unique_ptr<Operator> ops[6]; juce::ADSR adsr1, adsr2; VoiceParams params; float noteHz = 0.0f, lfoPhases[2] = {0.0f, 0.0f};
 };
